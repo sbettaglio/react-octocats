f2b19580a7ed6e26133e4a36539d786c2eed5991
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-18 15:16:03 -0400

    completed adventure mode

---
+++ src/components/PageFooter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PageFooter extends Component {
+  render() {
+    return (
+      <>
+        <div>© 2013-2020 GitHub, Inc. All rights reserved.</div>
+      </>
+    )
+  }
+}
+
+export default PageFooter
